17d54ef11c1b80f7aa620c671d39b7613571fabe
Alexandre Dessoly <alexandre.dessoly1@gmail.com>
2022-04-28 14:56:46 +0200

    formatage heure et creation switch pour le statut du parking

--- src/main/java/com/formation/parking/services/impl/ParkingServiceImpl.java
+++ src/main/java/com/formation/parking/services/impl/ParkingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.formation.parking.services.impl;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class ParkingServiceImpl implements ParkingService {
 
 	@Autowired
 	public ParkingAPIDAO parkingAPIDAO;
-	
+
 	@Override
 	public List<Parking> getListeParkings() {
 		ReponseParkingAPIEntity reponse = parkingAPIDAO.getListeParkings();
@@ -26,16 +28,38 @@ public class ParkingServiceImpl implements ParkingService {
 
 	private List<Parking> transformEntityToModel(ReponseParkingAPIEntity reponse) {
 		List<Parking> resultat = new ArrayList<Parking>();
-		for(RecordEntity record: reponse.getRecords()) {
+		for (RecordEntity record : reponse.getRecords()) {
 			Parking parking = new Parking();
 			parking.setNom(record.getFields().getGrpNom());
-			parking.setStatut(record.getFields().getGrpStatut());
+			parking.setStatut(getLibelleStatut(record));
 			parking.setNbPlaceDispo(record.getFields().getGrpDisponible());
 			parking.setNbPlaceTotal(record.getFields().getGrpExploitation());
-			parking.setHeureMaj(record.getFields().getGrpHorodatage());
+			parking.setHeureMaj(getHeurMaj(record));
 			resultat.add(parking);
 		}
 		return resultat;
+	}
+
+	private String getHeurMaj(RecordEntity record) {
+		OffsetDateTime dateMaj = OffsetDateTime.parse(record.getFields().getGrpHorodatage());
+		OffsetDateTime dateMajWithOffsetPlus1 = dateMaj.withOffsetSameInstant(ZoneOffset.of("+01:00"));
+		return dateMajWithOffsetPlus1.getHour()+ "h"+dateMajWithOffsetPlus1.getMinute() ;
+	}
+
+	private String getLibelleStatut(RecordEntity record) {
+		switch (record.getFields().getGrpStatut()) {
+			case "1": {
+				return "FERME";
+			}
+			case "2": {
+				return "ABONNES";
+			}
+			case "5": {
+				return "OUVERT";
+			}
+		}
+		return "Données non disponibles";
+
 	}
 
 }
